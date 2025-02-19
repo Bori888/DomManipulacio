af0b605c21e857e2c5f69c84f63568b756bbd656
Bori888 <ilyes.bori8@gmail.com>
2025-02-19 11:11:25 +0100

    fugvények+felxbox

--- main.js
+++ main.js
@@ -8,3 +8,39 @@ let tartalomElem =document.getElementById("tartalom")
 diakMegjelenit(diak1,tartalomElem)
 diakokMegjelenit(diakokLista,tartalomElem)
 
+/* elem eleérése */
+
+console.log(tartalomElem)
+let diakElemek =document.getElementsByClassName("diak")
+console.log(diakElemek);
+console.log(diakElemek[2]);
+
+let diakElemek2 =document.querySelector(".diak")
+console.log(diakElemek2);
+
+let diakElemek3 =document.querySelectorAll(".diak")
+console.log(diakElemek3);
+console.log(diakElemek3[0]);
+
+let articleElem =document.querySelectorAll("article")
+console.log(articleElem);
+console.log(articleElem[0]);
+
+let inputElem =document.querySelectorAll("#nev")
+console.log(inputElem);
+console.log(inputElem[0]);
+
+diakElemek3[2].addEventListener("click",fnev)
+function fnev(event) {
+    console.log(event.target);
+    console.log(diakElemek3[2]);
+    let asideElem =document.querySelectorAll("aside")
+    console.log(articleElem);
+    articleElem[0].innerHTML+= diakElemek3[2].innerHTML
+    
+    
+    
+    
+}
+
+
